5a6e8330d61c5f820c91316236fc8dcd26fcb4e8
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-13 14:14:16 -0700

    add UI logic to allow for multiple items to be added
    Co-authored-by: PJ Perry <pjperry28@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using ToDoList.Models;
 
 
 namespace ToDoList.Models
@@ -9,28 +8,37 @@ namespace ToDoList.Models
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to your to do list!");
-      Console.WriteLine("Would you like to add an item or view your list? answer with add or view");
-      string response1 = Console.ReadLine();
-      if (response1 == "add")
-      {
-        Console.WriteLine("what is the description of this list item?");
+      Console.WriteLine("Welcome to the To Do list!");
+      string Response1 = "";
+       while(Response1 != "end")
+        {
+          Console.WriteLine("Would you like to add an item or view your list?");
+          Console.WriteLine("Answer with add, view, or end");
+          Response1 = Console.ReadLine();
+
+          if (Response1 == "add")
+          {
+            Add();
+          }
+          else if (Response1 == "view")
+          {
+            View();
+          }
+        }    
+    }
+    public static void Add()
+    {
+      Console.WriteLine("what is the description of this list item?");
         string addResponse1 = Console.ReadLine();
         Item addResponse = new Item(addResponse1);
-        Console.WriteLine(addResponse.Description + " has been added to your list, would you like to add an item to your list or view you list, reply with add or view.");
-        string addResponse2 = Console.ReadLine();
-        if (addResponse2 == "view")
-        {
-          List<Item> ItemList = Item.GetAll();
-          for(int i = 0; i <= ItemList.Count -1; i++)
+    }  
+    public static void View()
+    {
+      List<Item> ItemList = Item.GetAll();
+        for(int i = 0; i <= ItemList.Count - 1; i++)
           {
-            Console.WriteLine(ItemList[i].Description);
-          }  
-        } else if (addResponse2 == "add")
-        {
-          Main(); 
-        }
-      }   
-    }
+            Console.WriteLine((i + 1) + ". " + ItemList[i].Description);
+          } 
+    } 
   }
 }
