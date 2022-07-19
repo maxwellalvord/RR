54d40ea973bb44a88db201864ff711675cbfd9ff
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-19 09:41:59 -0700

    add updated code to guthub

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,13 +1,18 @@
 
 using System.Collections.Generic;
 
-
 namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description { get; set;}
+    public string Description { get; set; }
     private static List<Item> _instances = new List<Item> {};
+
+    public Item (string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
 
     public static List<Item> GetAll()
     {
@@ -18,10 +23,36 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
-    public Item(string description)
-    {
-      Description = description;
-      _instances.Add(this);
-    }
+
   }
 }
+
+
+
+// using System.Collections.Generic;
+
+// namespace ToDoList.Models
+// {
+//   public class Item
+//   {
+//     public string Description { get; set; }
+//     private static List<Item> _instances = new List<Item> {};
+
+//     public Item (string description)
+//     {
+//       Description = description;
+//       _instances.Add(this);
+//     }
+
+//     public static List<Item> GetAll()
+//     {
+//       return _instances;
+//     }
+
+//     public static void ClearAll()
+//     {
+//       _instances.Clear();
+//     }
+
+//   }
+// }
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,51 +1,78 @@
-using System;
-using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-
-namespace ToDoList.Models
+namespace ToDoList
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to the To Do list!");
-      string Response1 = "";
-       while(Response1.ToLower() != "end")
-        {
-          Console.WriteLine("Would you like to add an item or view your list?");
-          Console.WriteLine("Answer with add, view, or end");
-          Response1 = Console.ReadLine();
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-          if (Response1.ToLower() == "add")
-          {
-            Add();
-          }
-          else if (Response1.ToLower() == "view")
-          {
-            View();
-          }
-        }    
+      host.Run();
     }
-    public static void Add()
-    {
-      Console.WriteLine("what is the description of this list item?");
-        string addResponse1 = Console.ReadLine();
-        Item addResponse = new Item(addResponse1);
-    }  
-    public static void View()
-    {
-      List<Item> ItemList = Item.GetAll();
-      if (ItemList.Count <= 0 )
-      {
-        Console.WriteLine("Oh No! You haven't added anything to your to do list yet!");
-      }
-      else if (ItemList.Count >= 1)
-      {
-       for(int i = 0; i <= ItemList.Count - 1; i++)
-          {
-            Console.WriteLine((i + 1) + ". " + ItemList[i].Description);
-          }  
-      }
-    } 
   }
 }
+
+
+
+
+
+
+
+// using System;
+// using System.Collections.Generic;
+
+
+// namespace ToDoList.Models
+// {
+//   public class Program
+//   {
+//     public static void Main()
+//     {
+//       Console.WriteLine("Welcome to the To Do list!");
+//       string Response1 = "";
+//        while(Response1.ToLower() != "end")
+//         {
+//           Console.WriteLine("Would you like to add an item or view your list?");
+//           Console.WriteLine("Answer with add, view, or end");
+//           Response1 = Console.ReadLine();
+
+//           if (Response1.ToLower() == "add")
+//           {
+//             Add();
+//           }
+//           else if (Response1.ToLower() == "view")
+//           {
+//             View();
+//           }
+//         }    
+//     }
+//     public static void Add()
+//     {
+//       Console.WriteLine("what is the description of this list item?");
+//         string addResponse1 = Console.ReadLine();
+//         Item addResponse = new Item(addResponse1);
+//     }  
+//     public static void View()
+//     {
+//       List<Item> ItemList = Item.GetAll();
+//       if (ItemList.Count <= 0 )
+//       {
+//         Console.WriteLine("Oh No! You haven't added anything to your to do list yet!");
+//       }
+//       else if (ItemList.Count >= 1)
+//       {
+//        for(int i = 0; i <= ItemList.Count - 1; i++)
+//           {
+//             Console.WriteLine((i + 1) + ". " + ItemList[i].Description);
+//           }  
+//       }
+//     } 
+//   }
+// }
