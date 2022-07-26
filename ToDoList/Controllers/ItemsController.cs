96e072df5482d70475c45fedd0bfbc25b38d6004
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-26 14:07:16 -0700

    add eager loading

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -3,6 +3,7 @@ using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -17,19 +18,22 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-        List<Item> model = _db.Items.Include(item => item.Category).ToList();
-        return View(model);
+      List<Item> model = _db.Items.Include(item => item.Category).ToList(); 
+      ViewBag.PageTitle = "View All Items";
+      return View(model);
     }
 
     public ActionResult Create()
     {
-        return View();
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
     }
 
     public ActionResult Edit(int id)
     {
-        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-        return View(thisItem);
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
     }
 
     public ActionResult Delete(int id)
